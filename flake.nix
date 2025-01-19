22facbf9f78f19030a44ecd500c3133f694a0df7
geodic <geodic.github@proton.me>
2025-01-18 19:30:34 -0500

    Fixed flake

--- flake.nix
+++ flake.nix
@@ -6,16 +6,14 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
-  	let pkgs = import nixpkgs { system = "x86_64-linux"; config.allowUnfree = true; };
-  	in {
-      nixosConfigurations.advaiths-linux = pkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          # Import the previous configuration.nix we used,
-          # so the old configuration file still takes effect
-          ./configuration.nix
-        ];
+  outputs = { self, nixpkgs, ... }@inputs: {    
+    nixosConfigurations.advaiths-linux = pkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        # Import the previous configuration.nix we used,
+        # so the old configuration file still takes effect
+        ./configuration.nix
+      ];
     };
   };
 }
