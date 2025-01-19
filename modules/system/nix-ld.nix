9ffbb7d071aa3ef183a33a3f5eecfb65409a4b37
geodic <geodic.github@proton.me>
2025-01-19 14:03:01 -0500

    fix nix-ld

--- modules/system/nix-ld.nix
+++ modules/system/nix-ld.nix
@@ -2,4 +2,27 @@
 
 {
   programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+      acl
+      attr
+      bzip2
+      dbus
+      expat
+      fontconfig
+      freetype
+      fuse3
+      icu
+      libnotify
+      libsodium
+      libssh
+      libunwind
+      libusb1
+      libuuid
+      nspr
+      nss
+      stdenv.cc.cc
+      util-linux
+      zlib
+      zstd
+  ]
 }
