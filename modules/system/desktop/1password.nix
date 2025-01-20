9fb921e92369d130b3ff4bb0268aed0f993fd46c
geodic <geodic.github@proton.me>
2025-01-20 12:56:37 -0500

    added ssh-agent config

--- modules/system/desktop/1password.nix
+++ modules/system/desktop/1password.nix
@@ -2,4 +2,10 @@
 {
   programs._1password-gui.enable = true;
   programs._1password.enable = true;
+
+  # Configure ssh-agent
+  programs.ssh.extraConfig = ''
+  Host *
+	  IdentityAgent ~/.1password/agent.sock
+  '';
 }
